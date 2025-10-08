9eed4705ee4baa7d32a9cddfea6289dc6c121220
Razaan <razaanreza0705@gmail.com>
2025-10-09 02:04:50 +0600

    added search function

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 import AppCards from '../Components/AppCards'
 import useApps from '../Hooks/useApps'
 
 function Apps() {
+  const [search, setSearch] = useState('')
   const { apps } = useApps()
+  const word = search.trim().toLocaleLowerCase()
+  const searchedApps = word
+    ? apps.filter(app => app.title.toLocaleLowerCase().includes(word))
+    : apps
   return (
     <div className="bg-[#f5f5f5] py-10">
       <div className="px-14">
@@ -11,10 +17,8 @@ function Apps() {
           Explore All Apps on the Market developed by us. We code for Millions
         </p>
       </div>
-      <div className='px-14 pb-2 flex justify-between items-center'>
-        <h1 className="font-bold text-lg">
-          ({apps.length}) Apps Found
-        </h1>
+      <div className="px-14 pb-2 flex justify-between items-center">
+        <h1 className="font-bold text-lg">({searchedApps.length}) Apps Found</h1>
         <label className="input right-3">
           <svg
             className="h-[1.5em] opacity-50"
@@ -32,11 +36,17 @@ function Apps() {
               <path d="m21 21-4.3-4.3"></path>
             </g>
           </svg>
-          <input type="search" required placeholder="search Apps" />
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="search"
+            required
+            placeholder="search Apps"
+          />
         </label>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 px-14 bg-[#f5f5f5]">
-        {apps.map((app) => (
+        {searchedApps.map((app) => (
           <AppCards key={app.id} app={app}></AppCards>
         ))}
       </div>
